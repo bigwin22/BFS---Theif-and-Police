fa7c9dae1d93a2c1f2404a89769a45d3f8f23928
bigwin22 <gogletaehyung0808@naver.com>
2022-07-22 19:29:32 +0900

    last

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
          돈과 함께 경찰의 추적을 피하고자 한다. 성민이가 경찰의 추적을 피할 수 있는지 알아보자.
 
 입력 조건: 첫번째 줄에는 배열의 크기 n, m이 주어진다.
-        2번째 줄부터 n번째 줄까지는 성민이와 성민이와 집, 경찰의 위치가 각각 0, 1, 2로 주어진다.
+        2번째 줄부터 n번째 줄까지는 성민이와 성민이와 집, 경찰의 위치가 각각 t, h, p로 주어진다.
         빈 공간은 . 이며 장애물은 x 이다.
         성민이와 성민이와 집은 서로 다른 위치를 가진다.
 
@@ -31,16 +31,19 @@ int hx, hy;//집의 위치
 int tmin_move = 9999999;//도둑 최소 이동 횟수
 int ptmin_move = 9999999;//경찰 최소 이동 횟수
 
-void bfs(int px, int py, int tx, int ty, int cnt) //px, py : 경찰의 좌표, tx, ty : 도둑의 좌표, hx, hy: 집의 좌표
-//1을 리턴하는 경우는 도둑이 무사히 집에 도착할 수 있는 경우이다.
-//1은 정상적으로 도착을 한 경우이다.
-//만약 진행도중 cnt가 최소 이동 횟수를 넘으면 그 함수는 진행하지 않는다.
-//만약 경찰이 도둑의 집까지 도착하는 최소 이동 횟수보다 적거나 같으면 도둑은 집에 못 온다. -> 동시에 도착하면 도둑의 패배
-
-//즉 이 문제는 도둑과 경찰의 최소를 겨루는 것이 쟁점이다.
+int checking(int px, int py, int tx, int ty, int cnt)
 {
-    if ((px > m || py > n || px < 1 || py < 1) || (tx > m || ty > n || tx < 1 || ty < 1))//좌표의 범위 유효성 검사
-        return;
+    /* 
+    다음 좌표의 유효성 검사: 
+    리턴 값이 0일 경우 둘의 모두 좌표에 문제가
+    리턴 값이 -1일 경우 도둑의 좌표에 문제가
+    리턴 값이 -2일 경우 경찰의 좌표에 문제가
+    리턴 값이 1일 경우 둘의 좌표는 유효하다
+    */
+    if ((px > m || py > n || px < 1 || py < 1))//좌표의 범위 유효성 검사(경찰) -> 불가의 경우
+        return -2;
+    if ((tx > m || ty > n || tx < 1 || ty < 1))//좌표의 범위 유효성 검사(도둑) -> 불가의 경우
+        return -1;
     if (arr[px][py] == 'x' || arr[tx][ty] == 'x')//장애물이 있는 경우
         return;
     if (pvisit[px][py] || tvisit[tx][ty])//경찰이나 도둑이 한번이라도 방문 했을 경우
@@ -62,11 +65,20 @@ void bfs(int px, int py, int tx, int ty, int cnt) //px, py : 경찰의 좌표, t
             ptmin_move = cnt;
         return;
     }
+}
 
-    
+void bfs(int px, int py, int tx, int ty, int cnt) //px, py : 경찰의 좌표, tx, ty : 도둑의 좌표, hx, hy: 집의 좌표
+//1을 리턴하는 경우는 도둑이 무사히 집에 도착할 수 있는 경우이다.
+//1은 정상적으로 도착을 한 경우이다.
+//만약 진행도중 cnt가 최소 이동 횟수를 넘으면 그 함수는 진행하지 않는다.
+//만약 경찰이 도둑의 집까지 도착하는 최소 이동 횟수보다 적거나 같으면 도둑은 집에 못 온다. -> 동시에 도착하면 도둑의 패배
 
-    int mx[4] = {-1, 1, 0, 0};// x 이동 좌표
-    int my[4] = {0,  0, -1, 1};// y 이동 좌표
+//즉 이 문제는 도둑과 경찰의 최소를 겨루는 것이 쟁점이다.
+{
+
+
+    int mx[4] = {0, 1, -1, 0};// x 이동 좌표
+    int my[4] = {1, 0, 0, -1};// y 이동 좌표
     
 
 
@@ -74,6 +86,9 @@ void bfs(int px, int py, int tx, int ty, int cnt) //px, py : 경찰의 좌표, t
     tvisit[tx][ty] = 1;
     for (int i = 1; i <= 4; i++)//재귀 함수 호출
     {
+        int ntx, nty;//다음 좌표
+        int npx, npy;
+
         for (int j = 1; j <= 4; j++)
         {
             bfs(px + mx[i - 1], py + my[i - 1], tx + mx[j - 1], ty + my[j - 1], cnt + 1);
@@ -97,17 +112,17 @@ int main()
         for (int j = 1; j <= m; j++)
         {
             scanf(" %c", &arr[i][j]);
-            if (arr[i][j] == '0')
+            if (arr[i][j] == 't')
             {
                 tx = i;
                 ty = j;
             }
-            else if (arr[i][j] == '1')
+            else if (arr[i][j] == 'h')
             {
                 hx = i;
                 hy = j;
             }
-            else if (arr[i][j] == '2')
+            else if (arr[i][j] == 'p')
             {
                 px = i;
                 py = j;
@@ -116,6 +131,7 @@ int main()
     }
 
     bfs(px, py, tx, ty, 0);//도둑이 집에 도착할 수 있는지 확인한다.
+    printf("\n%d %d", ptmin_move, tmin_move);
     if (tmin_move >= ptmin_move)//도둑이 집에 도착할 수 없는 경우이거나 집에 도착할 수 없는 경우
         printf("-1");
     else//도둑이 집에 도착할 수 있는 경우
