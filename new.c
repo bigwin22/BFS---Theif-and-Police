66204e89737398eaa59e7bc2c90128fcc8629883
bigwin22 <gogletaehyung0808@naver.com>
2022-08-05 00:16:46 +0900

    Update new.c
    1. que input and output 수정됨
    2. checking 함수 사전 주석 작성(추가 검토 필요)
    3. BFS를 수정하고 작동 방식 확립
       -이 함수의 작동 방식을 for문 내에서 다음에 호출할 함수들을 미리 다 검토하고 return할 때는 return/위치 체크 역시 for문 내에서 하는 것으로 규정함(추후 검토 후 수정해야함, 이 코드에는 반영 안되어 있음)/현재 호출된 함수는 단순히 자신의 위치를 초기화 하고 큐에서 다음 함수의 값을 불러와 호출하는 역할
    
    **이 모든 코드는 언제든지 수정될 수있음을 전제로 하는 코드들이다.(단 수정 가능이라고 적혀있는 부분들은 더 유심히 보고 다른 코드들 보다 변동이 심할 수 있음을 의미함)

--- new.c
+++ new.c
@@ -27,51 +27,93 @@ int tvisit[10000][10000];//도둑의 위치 방문 여부
 
 int hx,hy;//집의 위치
 
-
-int tmin_move = 9999999;
-int ptmin_move = 9999999;
-
 int cnt_que[10000];
 int px_que[10000];
 int py_que[10000];
 int tx_que[10000];
-int ty_que[10000];//값들의 큐(근데 이거 함수 포인터 쓰면 되는지 알아봐)
-int qi = 0, qj = 1; //qi:빼내야할 값이 있는 번호
+int ty_que[10000];//값들의 큐
+int qi = 0, qj = 1; //qi:빼내야할 값이 있는 번호, qj: 값을 넣어야하는 위치
 
 void que_output(int *cnt, int *px, int *py, int *tx, int *ty)
 {
-    cnt = cnt_que[qi];
-    
+    /*
+        큐에서 값을 빼내어 변수에 대입 시켜줌
+    */
+
+    if (cnt_que[qi] == -1) return;
+    *cnt = cnt_que[qi];
+    cnt_que[qi] = -1;//-1인 이유는 0도 좌표가 가능하기 때문에 이것을 설정해준다.
+    *px = px_que[qi];
+    px_que[qi] = -1;
+    *py = py_que[qi];
+    py_que[qi] = -1;
+    *tx = tx_que[qi];
+    tx_que[qi] = -1;
+    *ty = ty_que[qi];
+    qi = (qi + 1) % 10000;
 }
 
 void que_input(int cnt, int px, int py, int tx, int ty)
 {
+    /*
+        큐에 값을 넣어줌
+    */
 
+    if (cnt_que[qj] != -1)
+    {
+        printf("오류\n");
+        return;
+    }//큐가 가득 찼을 경우 오류(Only for debug)
+    
+    cnt_que[qj] = cnt;
+    px_que[qj] = px;
+    py_que[qj] = py;
+    tx_que[qj] = tx;
+    ty_que[qj] = ty;
+    qj = (qj + 1) % 10000;
 }
 
 int checking(int px, int py, int tx, int ty)
 {
-
+    /* 반환의 종류
+        1. 경찰과 도둑 모두 이상 없음
+        0. 둘 다 이상 있음
+        -1. 경찰만 이상 있음
+        -2. 도둑만 이상 있음
+    */
+    /* 반환의 경우
+        1. 도둑이나 경찰이 범위를 벗어나는 경우(이 경우는 무조건 거짓을 반환한다.)
+        2. 도둑이나 경찰 둘 중 하나라도 현재 위치에 이미 방문한 경우(이 경우는 무조건 거짓을 반환한다.)
+        3. 도둑이나 경찰이 장애물을 만나는 경우(한명이라도 이동 가능하면 참을 반환한다.)
+        4. 경찰이 집에 도착했을 경우(이 경우는 무조건 거짓을 반환한다.)
+        5. 도둑이 집에 도착했을 경우(이 경우는 무조건 참을 반환한다.)
+    */
 }
 
 int BFS(int cnt, int px, int py, int tx, int ty)
 {
-    if (checking(px, py, tx, ty)) return 0;
-
     int mx[4] = {-1,1,0,0};
     int my[4] = {0,0,-1,1};//이동 좌표(상하좌우)
 
-    pvisit[px][py] = 1;
-    tvisit[tx][ty] = 1;
+  
     for (int i = 0 ; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            int ncnt, npx, npy, ntx, nty;//다음 값들
-            que_output(ncnt, npx, ntx, nty);//값 대입
-        }
+            if (checking(px+mx[i], py+my[i], tx+mx[j], ty+my[j]) == 0) continue;
+            else return cnt+1;//집에 한번이라도 도착할 수 있는 경우가 최단거리이다.
 
-    }
+            //여기까지 오면 가볼만한 구간이다(이 코드는 조건부를 수정 할 경우 checking if문 안으로 경우에 맞춰 넣어줘야한다.)
+            que_input(cnt+1, px+mx[i], py+my[i], tx+mx[j], ty+my[j]);
+        }
+    }  
+
+    pvisit[px][py] = 0;
+    tvisit[tx][ty] = 0;//방문 여부 초기화
+    int ncnt, npx, npy, ntx, nty;//다음 값들
+    que_output(ncnt, npx, npy, ntx, nty);//값 대입
+    return BFS(ncnt, npx, npy, ntx, nty);//다음 값으로 반복
+
 }
 
 int main()
@@ -112,4 +154,5 @@ int main()
     {
         printf("%d",val);
     }
+    return 0;
 }
